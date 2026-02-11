318010c98d58e4ededadd39fe1ca7bf81362068f
Elin Ronda <elro2506@student.miun.se>
2026-02-11 09:58:07 +0100

    Tar tillbaka gammal javascript pga blockering i andra länken

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 "use strict";
 
-
 //När hela HTML-dokumntet laddats så körs loadCountries
 document.addEventListener("DOMContentLoaded", () => {
   loadCountries(); //Hämtar länder så jag kan skriva ut dem i puffen
 
-
 });
   //Här gör jag en fetch på API/kursdatan och ser till så att det returneras som javascript
   async function fetchCountries() {
-    const response = await fetch('https://jsonlint.com/datasets/continents.json');
+    const response = await fetch('https://raw.githubusercontent.com/Ginden/capitals/master/europe.json');
     if (!response.ok) { //Kollar så servern svarar ok
       throw new Error('Kan inte hämta kursdata'); //Om servern inte svarar ok så skrivs detta ut
     }
@@ -20,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   async function loadCountries() {
     try {
       const data = await fetchCountries(); //Hämtar länder från dokumentet
-      fillList(data.continents);  //Sparar länderna globalt
+      fillList(data);  //Sparar länderna globalt
     }
     catch (error) {
       console.error("Det uppstod ett fel:", error.message);
@@ -28,15 +26,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   //Funktion för attfylla listan
-  function fillList(continents) {
+  function fillList(countries) {
     const puff = document.querySelector(".puff"); //punkten framför är pga att det är en klass och inte element
     if (!puff) return;
     const ul = document.createElement("ul");
 
     //Gör en loop för varje land och skapar en rad i listan
-    continents.forEach((continent) => {
+    countries.forEach(item => {
       const li = document.createElement("li");
-      li.textContent = `${continent.name}: ${continent.largest_country ?? "-"}`;
+      li.textContent = `${item.properties.country}: ${item.properties.capital}`;
       ul.appendChild(li); //Lägger till li i ul
     });
   
@@ -44,4 +42,5 @@ document.addEventListener("DOMContentLoaded", () => {
   puff.appendChild(ul);
 
 }
+
 
