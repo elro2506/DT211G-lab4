e0814a6fb00e1995086d08abd773a54cd3c42cca
Elin Ronda <elro2506@student.miun.se>
2026-02-16 10:14:21 +0100

    Igångsättande av nytt projekt

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-    base: "/DT211G-lab3/",
+    base: "/DT211G-lab4/",
 
     build: {
         rollupOptions: {
             input: {
                 start: resolve(__dirname, "index.html"),
                 SASS: resolve(__dirname, "SASS.html"),
-                meny3: resolve(__dirname, "meny3.html"),
+                animations: resolve(__dirname, "animations.html"),
                 meny4: resolve(__dirname, "meny4.html"),
                 meny5: resolve(__dirname, "meny5.html")
             }
